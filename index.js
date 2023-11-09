06e503613a8fd004712bc643427404668aa74218
Kelly Reyes <kellyreyes219@gmail.com>
2023-11-08 23:22:27 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        return someValue - 42 
+    } 
+    else{ 
+        return 42 - someValue
+    }
+
+}
+
+function  distanceFromHqInFeet(someValue) {
+    if (someValue > 42) {
+        return (someValue - 42) * 264
+    } 
+    else{ 
+        return (42 - someValue) * 264
+    }
+}
+    
+function distanceTravelledInFeet(start, destination) { 
+    if (start && destination) { 
+        return Math.abs(start - destination) * 264;
+    }
+    else{ 
+        return (destination - start) * 264
+    }
+    
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = Math.abs(start - destination) * 264
+
+    if (distance <= 400) {
+        return 0;
+    }
+    else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    }
+    else if (distance > 2000 && distance < 2500) {
+        return 25;
+    }
+    else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+
+}  
+ 
+    
+
+
+
+
+
+
+ 
+
+ 
--- test/indexTest.js
+++ test/indexTest.js
@@ -49,7 +49,6 @@ describe('index.js', function() {
     });
 
     it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
-      // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
       expect(calculatesFarePrice(34, 32)).to.equal(2.56);
     });
 
